c37b8190476523276537c616335419f111e9d70b
Sean Law <seanmylaw@gmail.com>
2018-01-10 12:26:05 -0500

    Added Pins for all of the white LEDs

--- PinMapping.h
+++ PinMapping.h
@@ -1,17 +1,19 @@
-#define INTLED5 0 // Pin 20
-#define INTLED4 1 // Pin 19 
-#define INTLED2 22 // Pin 17
+#define INTLED0 7 // Pin 15
 #define INTLED1 23 // Pin 16
+#define INTLED2 22 // Pin 17
+#define INTLED3 20 // Pin 18
+#define INTLED4 1 // Pin 19
+#define INTLED5 0 // Pin 20
 
 #define INTLEDR 10 // Pin 12
 #define INTLEDG 5 // Pin 13
 #define INTLEDB 13 // Pin 14
 
-#define RX 2
-#define TX 3
+#define RX 8  
+#define TX 19
 
-#define 11 2 
-#define 10 3 
+#define SDA11 3 
+#define SCL10 2 
 
 #define A7 6 
 #define A4 8 
