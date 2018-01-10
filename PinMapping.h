13de4e88ad9310c76c712abcfa8a502971c0a3ac
Sean Law <seanmylaw@gmail.com>
2018-01-10 10:12:56 -0500

    Adding pin mapping header file for easy include

---
+++ PinMapping.h
@@ -0,0 +1,24 @@
+#define INTLED5 0 // Pin 20
+#define INTLED4 1 // Pin 19 
+#define INTLED2 22 // Pin 17
+#define INTLED1 23 // Pin 16
+
+#define INTLEDR 10 // Pin 12
+#define INTLEDG 5 // Pin 13
+#define INTLEDB 13 // Pin 14
+
+#define RX 2
+#define TX 3
+
+#define 11 2 
+#define 10 3 
+
+#define A7 6 
+#define A4 8 
+#define A8 9 
+#define A6 11 
+
+#define A9 19 
+#define A5 21 
+#define A2 24 
+#define A7 25 
